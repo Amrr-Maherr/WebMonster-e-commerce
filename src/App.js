049be8d75e103c6c2fb91828e6ae06de02585d68
Amr maher <amrm80233@>
2025-02-26 12:39:23 +0200

    feat: add MainNav component with Framer Motion animation
    - Implemented a navigation bar using Bootstrap for styling.
    - Added Framer Motion animation for smooth fade-in and slide-down effect.
    - Used React Router's Link component for navigation instead of href.
    - Included FontAwesome icons for wishlist and cart.
    - Improved class handling by replacing `class` with `className`.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { BrowserRouter } from 'react-router-dom';
-import SubNav from './Component/SubNav';
 function App() {
   return (
     <>
       <BrowserRouter>
         <div className="App">
-          <SubNav/>
+          
         </div>
       </BrowserRouter>
     </>
