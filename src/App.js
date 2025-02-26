9d9f5665e25a074e8d16d1b7c76349a28c63bb7d
Amr maher <amrm80233@>
2025-02-26 12:20:58 +0200

    feat: Add animated SubNav component using Framer Motion
    - Implemented the `SubNav` component with Framer Motion for smooth entrance animation.
    - The navigation bar now appears with a fade-in and slide-down effect.
    - Language selection dropdown includes Arabic, English, and French options.
    - Styled with `subnav-header`, `subnav-container`, and related classes.
    
    This update enhances the user experience with a dynamic visual effect.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { BrowserRouter } from 'react-router-dom';
+import SubNav from './Component/SubNav';
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <BrowserRouter>
+        <div className="App">
+          <SubNav/>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Component/SubNav.jsx
@@ -0,0 +1,35 @@
+import { motion } from "framer-motion";
+import "../Style/SubNav.css";
+
+export default function SubNav() {
+  return (
+    <motion.header
+      className="subnav-header"
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }} 
+      transition={{ duration: 0.5 }}
+    >
+      <nav className="subnav-nav">
+        <div className="subnav-container">
+          <p className="subnav-text p-0 m-0">
+            Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+          </p>
+          <a href="/" className="subnav-link">
+            Shop Now
+          </a>
+        </div>
+        <div className="subnav-select">
+          <select
+            name="language"
+            id="language-select"
+            className="subnav-language"
+          >
+            <option value="ar">Arabic</option>
+            <option value="en">English</option>
+            <option value="fr">French</option>
+          </select>
+        </div>
+      </nav>
+    </motion.header>
+  );
+}
